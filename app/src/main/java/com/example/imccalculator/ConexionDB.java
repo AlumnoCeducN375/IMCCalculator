1ea7c1f73b7c89ffba378a94a174dc93c53a20a4
Ceduc <amedelw@alumnos.ceduc.cl>
2022-12-05 12:00:42 -0300

    IMC Calculadora

--- app/src/main/java/com/example/imccalculator/ConexionDB.java
+++ app/src/main/java/com/example/imccalculator/ConexionDB.java
@@ -14,6 +14,7 @@ public class ConexionDB extends SQLiteOpenHelper
         super(context, name, factory, version);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table usuarioIMC (nombre varchar (50) not null primary key, peso varchar(50), altura varchar(50),imc varchar(50))");
